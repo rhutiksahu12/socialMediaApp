1d8a52d672393a7467d178f595e0317de8a95c6c
Rhutik Sahu <shubhamsahu938@gmail.com>
2021-12-31 01:05:41 +0100

    submission

---
+++ app_backend/schemas/save.js
@@ -0,0 +1,16 @@
+export default {
+    name: 'save',
+    title: 'Save',
+    type: 'document',
+    fields: [{
+        name: 'postedBy',
+        title: 'postedBy',
+        type: 'postedBy'
+    },
+    {
+        name: 'userId',
+        title: 'UserID',
+        type: 'string'
+     },
+    ]
+}
